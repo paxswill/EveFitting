e623ca828f8ef439b425d356de8aec257b6310ed
Will Ross <paxswill@gmail.com>
2013-07-22 19:24:29 -0400

    Add CCP copyright notice

--- EveFittingMapArray.php
+++ EveFittingMapArray.php
@@ -26,6 +26,14 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  *
+ * EVE Online and the EVE logo are the registered trademarks of CCP hf. All
+ * rights are reserved worldwide. All other trademarks are the property of
+ * their respective owners. EVE Online, the EVE logo, EVE and all associated
+ * logos and designs are the intellectual property of CCP hf. All artwork,
+ * screenshots, characters, vehicles, storylines, world facts or other
+ * recognizable features of the intellectual property relating to these
+ * trademarks are likewise the intellectual property of CCP hf.
+ *
  * @file
  */
 
